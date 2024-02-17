48d7acd06ceee4bb389eb564fc4706e06281011b
klodmone <kladmone54@gmail.com>
2024-02-17 09:38:34 +0100

    Summary

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,10 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;
+
+type DisableOrEnable = "enable" | "disable";
+
+let literal: DisableOrEnable;
+literal = "enable";
+literal = "disable";
--- src/generic/8.ts
+++ src/generic/8.ts
@@ -2,7 +2,6 @@
   У вас є тип Form, який містить інформацію про форму, включаючи поле errors. 
   Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
 */
-
 type Errors = {
   email?: string[];
   firstName?: string[];
@@ -18,7 +17,6 @@ type Form = {
   errors: Errors;
 };
 
-// Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+type Params = Omit<Form, "errors">;
 
 export {};
